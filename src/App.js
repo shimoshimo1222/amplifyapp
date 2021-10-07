2c2a3bd96ffc339a7a51b935c87846f6eded62d2
shimoshimo1222 <shimoshimo1222@gmail.com>
2021-10-07 22:12:10 +0900

    fourth

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App" >
       <header>
         <img src={logo} className="App-logo" alt="logo"/>
-        <h1>認証画面できたのになぜかビルドできないんだ？</h1>
+        <h1>認証画面できたのになぜかビルドできないんだ？？？</h1>
       </header>
       <AmplifySignOut/>
     </div>
