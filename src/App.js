ba45c29936a4e3d72026a40f466fe968330d15cd
shimoshimo1222 <shimoshimo1222@shimoshimo1222noMacBook-Pro.local>
2021-10-07 21:53:16 +0900

    add auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {withAuthenticator,AmplifySignOut} from '@aws-amplify/ui-react'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Amplifyでデプロイしてやったぜ
-        </a>
+    <div className="App" >
+      <header>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1>認証画面できたか・・・？？</h1>
       </header>
+      <AmplifySignOut/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
